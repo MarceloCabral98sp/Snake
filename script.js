975f4158c3f2f52a0c1240f49a895d1f9c049210
MarceloCabral98sp <marcelo.cabral98sp@gmail.com>
2020-01-20 23:54:58 -0300

    creating snake moviment

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 //inicia o canvas
 function criarBG(){
@@ -21,5 +22,67 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+// usando o evento do teclado para chamar a function update
+document.addEventListener("keydown", update);
+
+function update(event){
+
+    if(event.keyCode == 37 && direction != "right"){
+        direction = "left";
+    }
+    else if(event.keyCode == 38 && direction != "down"){
+        direction = "up";
+    }
+    else if(event.keyCode == 39 && direction != "left"){
+        direction = "right";
+    }
+    else if(event.keyCode == 40 && direction != "up"){
+        direction = "down";
+    }
+}
+
+function iniciarJogo(){
+
+    // a posição snake 0 é a cabeça da cobrinha
+    if(snake[0].x > 15 * box && direction == "right"){
+        snake[0].x = 0;
+    }
+    else if(snake[0].x < 0 && direction == "left"){
+        snake[0].x = 16 * box;
+    }
+    if(snake[0].y > 15 * box && direction == "down"){
+        snake[0].y = 0;
+    }
+    else if(snake[0].y < 0 && direction == "up"){
+        snake[0].y = 16 * box;
+    }
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x; 
+    let snakeY = snake[0].y;
+
+    if(direction == "right"){
+        snakeX += box;
+    }
+    else if(direction == "left"){
+        snakeX -= box;
+    }
+    else if(direction == "up"){
+        snakeY -= box;
+    }
+    else if(direction == "down"){
+        snakeY += box;
+    }
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+// a cada 100 milisegundos a função iniciar jogo é chamada novamente
+let jogo = setInterval(iniciarJogo, 100);
